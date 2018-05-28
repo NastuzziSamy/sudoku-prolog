c02bf87071f0a5bba1253fb97b2a773898207d6a
NastuzziSamy <samy@nastuzzi.fr>
2018-05-28 23:50:19 +0200

    Adaptation aux contraintes du sujet

--- sudoku.pl
+++ sudoku.pl
@@ -74,7 +74,8 @@ printLine([X|R]) :- write('|'), (var(X) -> write(' '); write(X)), printLine(R).
 printGrid([]).
 printGrid([Y|R]) :- printLine(Y), nl, printGrid(R).
 
-isLineOk(L) :- is_set(L).
+isInLine(X, [Y|T]) :- X = Y; isInLine(X, T).
+isLineOk(L)  :- setof(X, isInLine(X, L), S), length(L, N), length(S, N).
 
 isGridOk(L) :- L = [
         [A1, B1, C1, D1, E1, F1, G1, H1, I1],
@@ -89,34 +90,34 @@ isGridOk(L) :- L = [
     ],
 
     /* Vérification par ligne */
-    is_set([A1, B1, C1, D1, E1, F1, G1, H1, I1]),
-    is_set([A2, B2, C2, D2, E2, F2, G2, H2, I2]),
-    is_set([A3, B3, C3, D3, E3, F3, G3, H3, I3]),
-    is_set([A4, B4, C4, D4, E4, F4, G4, H4, I4]),
-    is_set([A5, B5, C5, D5, E5, F5, G5, H5, I5]),
-    is_set([A6, B6, C6, D6, E6, F6, G6, H6, I6]),
-    is_set([A7, B7, C7, D7, E7, F7, G7, H7, I7]),
-    is_set([A8, B8, C8, D8, E8, F8, G8, H8, I8]),
-    is_set([A9, B9, C9, D9, E9, F9, G9, H9, I9]),
+    isLineOk([A1, B1, C1, D1, E1, F1, G1, H1, I1]),
+    isLineOk([A2, B2, C2, D2, E2, F2, G2, H2, I2]),
+    isLineOk([A3, B3, C3, D3, E3, F3, G3, H3, I3]),
+    isLineOk([A4, B4, C4, D4, E4, F4, G4, H4, I4]),
+    isLineOk([A5, B5, C5, D5, E5, F5, G5, H5, I5]),
+    isLineOk([A6, B6, C6, D6, E6, F6, G6, H6, I6]),
+    isLineOk([A7, B7, C7, D7, E7, F7, G7, H7, I7]),
+    isLineOk([A8, B8, C8, D8, E8, F8, G8, H8, I8]),
+    isLineOk([A9, B9, C9, D9, E9, F9, G9, H9, I9]),
 
     /* Vérification par colonne */
-    is_set([A1, A2, A3, A4, A5, A6, A7, A8, A9]),
-    is_set([B1, B2, B3, B4, B5, B6, B7, B8, B9]),
-    is_set([C1, C2, C3, C4, C5, C6, C7, C8, C9]),
-    is_set([D1, D2, D3, D4, D5, D6, D7, D8, D9]),
-    is_set([E1, E2, E3, E4, E5, E6, E7, E8, E9]),
-    is_set([F1, F2, F3, F4, F5, F6, F7, F8, F9]),
-    is_set([G1, G2, G3, G4, G5, G6, G7, G8, G9]),
-    is_set([H1, H2, H3, H4, H5, H6, H7, H8, H9]),
-    is_set([I1, I2, I3, I4, I5, I6, I7, I8, I9]),
+    isLineOk([A1, A2, A3, A4, A5, A6, A7, A8, A9]),
+    isLineOk([B1, B2, B3, B4, B5, B6, B7, B8, B9]),
+    isLineOk([C1, C2, C3, C4, C5, C6, C7, C8, C9]),
+    isLineOk([D1, D2, D3, D4, D5, D6, D7, D8, D9]),
+    isLineOk([E1, E2, E3, E4, E5, E6, E7, E8, E9]),
+    isLineOk([F1, F2, F3, F4, F5, F6, F7, F8, F9]),
+    isLineOk([G1, G2, G3, G4, G5, G6, G7, G8, G9]),
+    isLineOk([H1, H2, H3, H4, H5, H6, H7, H8, H9]),
+    isLineOk([I1, I2, I3, I4, I5, I6, I7, I8, I9]),
 
     /* Vérification par bloc */
-    is_set([A1, A2, A3, B1, B2, B3, C1, C2, C3]),
-    is_set([D1, D2, D3, E1, E2, E3, F1, F2, F3]),
-    is_set([G1, G2, G3, H1, H2, H3, I1, I2, I3]),
-    is_set([A4, A5, A6, B4, B5, B6, C4, C5, C6]),
-    is_set([D4, D5, D6, E4, E5, E6, F4, F5, F6]),
-    is_set([G4, G5, G6, H4, H5, H6, I4, I5, I6]),
-    is_set([A7, A8, A9, B7, B8, B9, C7, C8, C9]),
-    is_set([D7, D8, D9, E7, E8, E9, F7, F8, F9]),
-    is_set([G7, G8, G9, H7, H8, H9, I7, I8, I9]).
+    isLineOk([A1, A2, A3, B1, B2, B3, C1, C2, C3]),
+    isLineOk([D1, D2, D3, E1, E2, E3, F1, F2, F3]),
+    isLineOk([G1, G2, G3, H1, H2, H3, I1, I2, I3]),
+    isLineOk([A4, A5, A6, B4, B5, B6, C4, C5, C6]),
+    isLineOk([D4, D5, D6, E4, E5, E6, F4, F5, F6]),
+    isLineOk([G4, G5, G6, H4, H5, H6, I4, I5, I6]),
+    isLineOk([A7, A8, A9, B7, B8, B9, C7, C8, C9]),
+    isLineOk([D7, D8, D9, E7, E8, E9, F7, F8, F9]),
+    isLineOk([G7, G8, G9, H7, H8, H9, I7, I8, I9]).
