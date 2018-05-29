1481c3f8764f72f6b48e0479894d0a9c0e51a7c4
NastuzziSamy <samy@nastuzzi.fr>
2018-05-29 12:12:11 +0200

    Ajout de la résolution

--- sudoku.pl
+++ sudoku.pl
@@ -38,13 +38,13 @@ grid2(L) :- L = [
 grid3(L) :- L = [
     [1, _, 3, _, 5, _, _, _, 9],
     [4, 5, _, _, 8, _, 1, 2, 3],
-    [_, 8, _, 1, 2, 3, 4, 5, 6],
+    [_, 8, _, 1, _, 3, _, 5, 6],
     [_, _, 4, 5, _, _, 8, _, 1],
-    [5, _, _, 8, _, 1, 2, 3, 4],
-    [8, _, 1, 2, 3, 4, 5, 6, 7],
-    [_, 4, 5, _, _, 8, _, 1, 2],
-    [_, _, 8, _, 1, 2, 3, 4, 5],
-    [_, 1, 2, 3, 4, 5, 6, 7, 8]
+    [5, _, _, 8, _, 1, _, _, 4],
+    [8, _, 1, 2, 3, 4, 5, _, 7],
+    [_, 4, 5, _, _, 8, _, _, 2],
+    [_, _, 8, _, 1, 2, _, _, 5],
+    [_, 1, 2, 3, _, 5, _, 7, 8]
 ].
 grid4(L) :- L = [
     [1, _, 3, _, 5, _, _, _, 9],
@@ -59,7 +59,7 @@ grid4(L) :- L = [
 ].
 
 /* La seconde valeur > 10 du random permet de ne pas populer toute la grille */
-tryToAffectValue(X, L) :- random(1, 11, Xr), (Xr < 10 -> X is Xr, isGridOk(L); !).
+tryToAffectValue(X, L) :- random(1, 40, Xr), (Xr < 10 -> X is Xr, isGridOk(L); !).
 
 randomLine([], _).
 randomLine([X|R], L) :- (tryToAffectValue(X, L) -> randomLine(R, L); randomLine([X|R], L)).
@@ -89,6 +89,8 @@ isGridOk(L) :- L = [
         [A8, B8, C8, D8, E8, F8, G8, H8, I8],
         [A9, B9, C9, D9, E9, F9, G9, H9, I9]
     ],
+
+    printGrid(L),
 
     /* Vérification par ligne */
     isLineOk([A1, B1, C1, D1, E1, F1, G1, H1, I1]),
@@ -122,3 +124,31 @@ isGridOk(L) :- L = [
     isLineOk([A7, A8, A9, B7, B8, B9, C7, C8, C9]),
     isLineOk([D7, D8, D9, E7, E8, E9, F7, F8, F9]),
     isLineOk([G7, G8, G9, H7, H8, H9, I7, I8, I9]).
+
+tryWith1(X, L) :- X is 1, isGridOk(L).
+tryWith2(X, L) :- X is 2, isGridOk(L).
+tryWith3(X, L) :- X is 3, isGridOk(L).
+tryWith4(X, L) :- X is 4, isGridOk(L).
+tryWith5(X, L) :- X is 5, isGridOk(L).
+tryWith6(X, L) :- X is 6, isGridOk(L).
+tryWith7(X, L) :- X is 7, isGridOk(L).
+tryWith8(X, L) :- X is 8, isGridOk(L).
+tryWith9(X, L) :- X is 9, isGridOk(L).
+
+tryToResolveValue(X, L) :- (nonvar(X) -> true;
+    (tryWith1(X, L) -> true;
+    (tryWith2(X, L) -> true;
+    (tryWith4(X, L) -> true;
+    (tryWith3(X, L) -> true;
+    (tryWith5(X, L) -> true;
+    (tryWith6(X, L) -> true;
+    (tryWith7(X, L) -> true;
+    (tryWith8(X, L) -> true;
+    (tryWith9(X, L))))))))))).
+
+resolveLine([], _).
+resolveLine([X|R], L) :- tryToResolveValue(X, L), resolveLine(R, L).
+
+resolveGrid([], _).
+resolveGrid([Y|R], L) :- isGridOk(L), resolveLine(Y, L), resolveGrid(R, L).
+resolveGrid(L) :- (isGridOk(L) -> printGrid(L), (resolveGrid(L, L) -> nl, write('Résolution:'), nl, printGrid(L); write('Grille insolvable'), false); write('Grille invalide'), false).
